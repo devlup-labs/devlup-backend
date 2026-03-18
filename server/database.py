5e3ac68a8ad2d01474a0418bd5c33294afcfe488
Parth Suryawanshi <parthsuryawanshi0207@gmail.com>
2026-03-18 23:36:28 +0530

    .

--- server/database.py
+++ server/database.py
@@ -12,11 +12,11 @@ client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
 
 
-project_collection = db["projects"]
-mentor_collection = db["mentors"]
-timeline_collection = db["timeline"]
+# project_collection = db["projects"]
+# mentor_collection = db["mentors"]
+# timeline_collection = db["timeline"]
 application_collection = db["applications"]
-preference_collection = db["application_preferences"]
-stats_collection = db["stats"]
-page_stats_collection = db["page_stats"]
-project_mentor_collection = db["project_mentors"]
+# preference_collection = db["application_preferences"]
+# stats_collection = db["stats"]
+# page_stats_collection = db["page_stats"]
+# project_mentor_collection = db["project_mentors"]
