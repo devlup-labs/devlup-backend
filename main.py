f3f81d0173933a7fa6a919ba01ba84ff6f22fb1b
Jainam-not-a-robot <b24cs1071@iitj.ac.in>
2026-06-25 16:08:00 +0530

    add backend url

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ from server.main_website.app import main_app
 app = Starlette(routes=[
     Host("projects.devluplabs.tech", app=projects_app),
     Host("devluplabs.tech", app=main_app),
-    Host("devlup-labs.vercel.app", app=main_app)
+    Host("devlup-labs.vercel.app", app=main_app),
+    Host("devlup-backend.onrender.com", app=projects_app),
 ])
